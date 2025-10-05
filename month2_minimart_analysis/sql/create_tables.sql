75ffa940cbb54688ec89baca7e0d2dcb6c63c302
Naelgebre10 <Naelgebre0@gmail.com>
2025-10-05 07:46:47 +0300

    Update create_tables.sql

--- month2_minimart_analysis/sql/create_tables.sql
+++ month2_minimart_analysis/sql/create_tables.sql
@@ -1,1 +1,21 @@
--- SQL script to create necessary tables
+-- SQL scrCREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(100),
+    join_date DATE
+);
+
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(50),
+    category VARCHAR(50),
+    price DECIMAL(10,2)
+);
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(customer_id),
+    product_id INT REFERENCES products(product_id),
+    quantity INT,
+    order_date DATE
+);ipt to create necessary tables
